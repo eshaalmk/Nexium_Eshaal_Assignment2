8828ff84c2737889554c4e268f6ee59ebd9cee1e
Eshaal <2005eshaal@gmail.com>
2025-07-14 15:04:58 +0500

    Fix MongoClient reassign error

--- internship/assignment-2/src/lib/mongo.ts
+++ internship/assignment-2/src/lib/mongo.ts
@@ -4,14 +4,11 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI!;
 const options = {};
 
-let client;
-let clientPromise: Promise<MongoClient>;
-
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
+const client = new MongoClient(uri, options);
+const clientPromise = client.connect();
 
 export default clientPromise;
